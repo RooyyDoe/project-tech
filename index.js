4f44ceee389714ab538864b0b0aa36c6e1b5396a
Roy Kuijper <roykuijper199716@hotmail.com>
2019-03-14 21:39:49 +0100

    Main images added

--- index.js
+++ index.js
@@ -3,22 +3,12 @@ const express = require('express')
 express() 
     .set('view engine', 'pug')
     .use('/static', express.static('static'))
-    .get('/about', about)
     .get('/', home)
-    .get('/login', login)
     .use(notFound)
     .listen(3000)
 
     function home(req, res) {
       res.render('index')
-    }
-
-    function about(req, res) {
-      res.render('about')
-    }
-
-    function login(req, res) {
-      res.render('login')
     }
 
 function notFound(req, res) {
