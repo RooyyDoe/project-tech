5e649e05af1c6bc3f6f2ac2834d62a798d4815e3
Roy Kuijper <roykuijper199716@hotmail.com>
2019-03-24 16:40:00 +0100

    Updated eslint file

--- routes/data.js
+++ routes/data.js
@@ -1,17 +1,17 @@
 const express = require('express');
 
 express() 
-    .set('view engine', 'pug')
-    .use('/static', express.static('static'))
-    .get('/', home)
-    .use(notFound)
-    .listen(3000);
+	.set('view engine', 'pug')
+	.use('/static', express.static('static'))
+	.get('/', home)
+	.use(notFound)
+	.listen(3000);
 
-    function home(req, res) {
-      res.render('../views/index.pug');
-    }
+function home(req, res) {
+	res.render('../views/index.pug');
+}
 
 function notFound(req, res) {
-  res.status(404).render('notfound.pug');
+	res.status(404).render('notfound.pug');
 }
 
